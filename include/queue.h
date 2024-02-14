e4045dd42f076f7cc3e9d0a65879adbfde0ca6d7
DaJourJChristophe <dc198jcn215n@gmail.com>
2024-02-14 11:23:03 -0800

    document the queue data structure core methods

--- include/queue.h
+++ include/queue.h
@@ -5,6 +5,12 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+/**
+ * @brief A safe implementation of a Queue data structure. This data
+ *        structure is based upon the Circular Buffer. It has a stateful
+ *        capacity specification and read and write pointers. Every
+ *        operation is a constant time operation.
+ */
 struct queue
 {
   int *items;
@@ -13,22 +19,70 @@ struct queue
   uint64_t w;
 };
 
+/**
+ * @brief An alias for the Queue data struct.
+ */
 typedef struct queue queue_t;
 
+/**
+ * @brief Allocate a new Queue data structure to the heap.
+ * @param cap The maximum capacity allow in the Queue data structure.
+ */
 queue_t *queue_new(const size_t cap);
 
+/**
+ * @brief Deallocate an existing Queue data structure from the heap.
+ * @param self A double pointer to the Queue container.
+ */
 void __queue_destroy(queue_t **self);
 
+/**
+ * @brief Create a stack-pointer and pass it to queue_destroy() so that
+ *        the queue pointer in the caller knows the queue no longer exists.
+ * @param self A pointer to the Queue container.
+ */
 #define queue_destroy(self) __queue_destroy(&self)
 
+/**
+ * @brief Add an item to the Queue data structure. By default, the Queue
+ *        adds items to the beginning of the data structure and removes
+ *        items from the end of the data structure.
+ *
+ *        @note In the future, an argument will be added to this method
+ *              so that end-users can change the queue scheme.
+ *
+ * @param self A pointer to the Queue container.
+ * @param item The item to be added to the Queue data structure.
+ * @return Whether or not the item was successfully added to the Queue.
+ */
 bool queue_enqueue(queue_t *self, const int item);
 
+/**
+ * @brief Remove an item from the Queue data structure.
+ * @param self A pointer to the Queue container.
+ * @return The item currently removed from the front of the Queue.
+ */
 int *queue_dequeue(queue_t *self);
 
+/**
+ * @brief Return the item at the front of the Queue data structure.
+ * @param self A pointer to the Queue container.
+ * @return A copy of the item currently at the front of the Queue.
+ */
 int *queue_peek(queue_t *self);
 
+/**
+ * @brief Return the number of items currently in the Queue data structure.
+ * @param self A pointer to the Queue container.
+ * @return The current size of the Queue data structure.
+ */
 size_t queue_size(queue_t *self);
 
+/**
+ * @brief Determine of the Queue data structure is empty.
+ * @param self A pointer to the Queue container.
+ * @return Whether or not the Queue data structure is empty.
+ */
 bool queue_empty(queue_t *self);
 
 #endif/*TURNPIKE__QUEUE_H*/
--- src/queue.c
+++ src/queue.c
@@ -5,14 +5,29 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+/**
+ * @brief Allocate the Queue container and the queue buffer to the heap.
+ * @note Do not implement your calls to the heap here. Instead, please use
+ *       the wrapper functions specified in common.h. These functions
+ *       deal with common problems like exception handling and fragmentation.
+ */
 static queue_t *queue_alloc(const size_t cap)
 {
   queue_t *self = NULL;
+  // TODO: Create a wrapper for this allocation call.
   self = (queue_t *)calloc(1, sizeof(*self));
+  // TODO: Create a wrapper for this allocation call.
   self->items = (int *)calloc(cap, sizeof(*self->items));
   return self;
 }
 
+/**
+ * @brief Allocate a new Queue data structure to the heap. Do not allocate
+ *        the queue properties here. Queue properties are allocated in
+ *        queue_alloc() refer to it for more information. With that, this
+ *        function sets up the Queue for usage.
+ * @param cap The maximum capacity allow in the Queue data structure.
+ */
 queue_t *queue_new(const size_t cap)
 {
   queue_t *self = NULL;
@@ -21,6 +36,10 @@ queue_t *queue_new(const size_t cap)
   return self;
 }
 
+/**
+ * @brief Deallocate an existing Queue data structure from the heap.
+ * @param self A double pointer to the Queue container.
+ */
 void __queue_destroy(queue_t **self)
 {
   if (self != NULL && *self != NULL)
@@ -31,6 +50,18 @@ void __queue_destroy(queue_t **self)
   }
 }
 
+/**
+ * @brief Add an item to the Queue data structure. By default, the Queue
+ *        adds items to the beginning of the data structure and removes
+ *        items from the end of the data structure.
+ *
+ *        @note In the future, an argument will be added to this method
+ *              so that end-users can change the queue scheme.
+ *
+ * @param self A pointer to the Queue container.
+ * @param item The item to be added to the Queue data structure.
+ * @return Whether or not the item was successfully added to the Queue.
+ */
 bool queue_enqueue(queue_t *self, const int item)
 {
   if (self == NULL)
@@ -47,6 +78,11 @@ bool queue_enqueue(queue_t *self, const int item)
   return true;
 }
 
+/**
+ * @brief Remove an item from the Queue data structure.
+ * @param self A pointer to the Queue container.
+ * @return The item currently removed from the front of the Queue.
+ */
 int *queue_dequeue(queue_t *self)
 {
   if (self == NULL)
@@ -65,6 +101,11 @@ int *queue_dequeue(queue_t *self)
   return item;
 }
 
+/**
+ * @brief Return the item at the front of the Queue data structure.
+ * @param self A pointer to the Queue container.
+ * @return A copy of the item currently at the front of the Queue.
+ */
 int *queue_peek(queue_t *self)
 {
   if (self == NULL)
@@ -83,6 +124,11 @@ int *queue_peek(queue_t *self)
   return item;
 }
 
+/**
+ * @brief Return the number of items currently in the Queue data structure.
+ * @param self A pointer to the Queue container.
+ * @return The current size of the Queue data structure.
+ */
 size_t queue_size(queue_t *self)
 {
   if (self == NULL)
@@ -93,6 +139,11 @@ size_t queue_size(queue_t *self)
   return (self->w - self->r);
 }
 
+/**
+ * @brief Determine of the Queue data structure is empty.
+ * @param self A pointer to the Queue container.
+ * @return Whether or not the Queue data structure is empty.
+ */
 bool queue_empty(queue_t *self)
 {
   if (self == NULL)
@@ -100,5 +151,7 @@ bool queue_empty(queue_t *self)
     return false;
   }
 
+  // Do not call the forward facing queue_size() method here.
+  // That method adds redundant overhead to this method call.
   return 0UL == (self->w - self->r);
 }
